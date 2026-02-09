0b138900b18170c1df670946bc3f68c16c2ba695
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2026-02-09 19:40:52 +0100

    Release v0.2.0

--- src/airflow_reservations/__init__.py
+++ src/airflow_reservations/__init__.py
@@ -5,6 +5,6 @@ from airflow_reservations.config import (
     load_config,
 )
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = ["get_reservation", "get_reservation_entry", "load_config", "__version__"]
